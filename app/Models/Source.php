f8512ca1d4d30070bbb821871a5e920a8441653f
Stephen <stolak>
2023-10-04 14:57:19 +0100

    Setting model and Migration

---
+++ app/Models/Source.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Source extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'source_key',
+        'source',
+    ];
+}
